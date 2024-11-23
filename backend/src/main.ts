5238c044429a62c35b9d04e433e1b438829fca89
liminfinity <108136720+liminfinity@users.noreply.github.com>
2024-11-23 19:26:32 +0700

    docs: add generating swagger yaml

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,9 @@ import { ENV, IS_DEV, ROUTER_PATHS } from '@/common/constants';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './common/configs';
 import { LoggerService } from './common/services';
+import { writeFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { dump } from 'js-yaml';
 
 const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule, {
@@ -33,6 +36,9 @@ const bootstrap = async (): Promise<void> => {
 
   if (IS_DEV) {
     const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+    const yamlDocument = dump(document);
+    writeFile(resolve('swagger.yaml'), yamlDocument);
 
     SwaggerModule.setup(ROUTER_PATHS.API, app, document);
   }
